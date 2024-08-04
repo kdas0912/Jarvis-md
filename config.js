b76f1cf47e044875b8e7c8a84196a63a2bf1945d
kdas0912 <kdas50300@gmail.com>
2024-08-04 08:43:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ module.exports = {
   AUDIO_DATA: process.env.AUDIO_DATA || 'ʟᴏᴋɪ-xᴇʀ;ᴊᴀʀᴠɪꜱ;https://i.imgur.com/ep3UbBP.jpg',
   WARN_COUNT: process.env.WARN_COUNT || '3',
   ALIVE_DATA : process.env.ALIVE_DATA || "_*Hy &sender i am alive now*_\n\n_PLATFORM: &platform_\n_RUNTIME : &runtime_\n\n_. type alive to update your alive message_",
-  SESSION_ID: process.env.SESSION_ID || 'Jarvis_c2b6_4636_9119_ee85_4498_4cf2_0ce6_7528',
+  SESSION_ID: process.env.SESSION_ID || 'Jarvis_b8d4_6630_fa1a_7cc5_a585_1c35_b0c2_f77a',
   LANG: process.env.LANG || 'EN',
   ELEVENLABS: process.env.ELEVENLABS,
   HANDLERS: process.env.HANDLER  || '.',
